4c02e2070ef03cec86ea439d81bde6c3ad6b01c9
WaltteriAXW <waltteri@axw.fi>
2025-05-12 14:46:54 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PolyurethaneOptimizer from './polyurethane_optimizer_component'
-import PythonCalculationErrorBoundary from './error_boundary'
+import AppComponent from './app_component'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PythonCalculationErrorBoundary>
-      <PolyurethaneOptimizer />
-    </PythonCalculationErrorBoundary>
+    <AppComponent />
   </React.StrictMode>,
 )
