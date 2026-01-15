c360c8b4bb03901f11265ce1b649bfe25abfd5ae
WaltteriAXW <waltteri@axw.fi>
2026-01-15 19:14:34 +0000

    Deploying to gh-pages from @ WaltteriAXW/Pu-optimizer-tool@d37219533071a20899ecdd08ea6183db00183b33 🚀

--- assets/index-pQWfPrjA.js
+++ assets/index-pQWfPrjA.js
@@ -18,17 +18,15 @@ Make sure Python files are synced to public/python/`;throw console.error(n),new 
 import json
 import traceback
 import sys
+import importlib
 
 _result = None
 try:
-    # Import the module dynamically
+    # Import the module dynamically using importlib
     module_path, func_name = '${s}'.rsplit('.', 1)
-    parts = module_path.split('.')
 
-    # Dynamic import
-    mod = __import__(parts[0])
-    for part in parts[1:]:
-        mod = getattr(mod, part)
+    # Use importlib for proper module imports
+    mod = importlib.import_module(module_path)
 
     # Get the function
     func = getattr(mod, func_name)
